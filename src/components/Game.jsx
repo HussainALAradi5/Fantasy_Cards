e4356e6e9678d93380e48bad1bd92ee6142bb6e5
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-08-09 17:05:09 +0300

    some styling  and some implmenetation in the cards

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,30 +1,40 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const Game = () => {
   const images = [
-    { src: "/img/angry_troll.png" },
-    { src: "/img/blu_happy.png" },
-    { src: "/img/brave_knight.png" },
-    { src: "/img/happy_knight_and_dragon.png" },
-    { src: "/img/knight_helmet.png" },
-    { src: "/img/knight.avif" },
-    { src: "/img/pink_happy.png" },
-  ];
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
+    { src: '/img/angry_troll.png' },
+    { src: '/img/blu_happy.png' },
+    { src: '/img/brave_knight.jpg' },
+    { src: '/img/happy_knight_and_dragon.jpg' },
+    { src: '/img/knight_helmet.png' },
+    { src: '/img/knight.avif' },
+    { src: '/img/pink_happy.png' }
+  ]
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
   const shuffle = () => {
     const shuffledCards = [...images, ...images]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
-    setCards(shuffledCards);
-    setTurns(0);
-  };
-  console.log("cards:", cards, "turns:", turns);
+      .map((card) => ({ ...card, id: Math.random() }))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  console.log('cards:', cards, 'turns:', turns)
 
   return (
     <div className="game">
       <button onClick={shuffle}>new game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div key={card.id} className="card">
+            <img src={card.src} className="front" alt="card-front" />
+            <img src="/img/cover.png" alt="card-back" className="back" />
+            <div></div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
-};
-export default Game;
+  )
+}
+export default Game
